0e1ede9c33e0672ff9cda53b2ebb1770fce1d54f
Simon Timms <stimms@gmail.com>
2013-12-03 14:49:58 -0700

    Initial add, already have bookmarks working

---
+++ DocX/Section.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace Novacode
+{
+  public class Section : Container
+  {
+
+    public SectionBreakType SectionBreakType;
+
+    internal Section(DocX document, XElement xml) : base(document, xml)
+    {
+    }
+
+    public List<Paragraph> SectionParagraphs { get; set; }
+  }
+}
